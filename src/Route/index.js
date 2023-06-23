f698f55f0e9d3dc421a18dbc1278239600a789b5
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-06-23 21:37:41 +0700

    Ganti route system

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { DashboardPage, ProjectsPage, UsersPage, LoginPage } from '../Pages';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {DashboardPage, ProjectsPage, UsersPage, LoginPage} from '../Pages';
 import ProtectedRoute from './ProtectedRoute';
 
 const index = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" exact component={LoginPage} />
+    return (<>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" exact component={LoginPage}/>
 
-          <ProtectedRoute path="/" component={DashboardPage} />
-          <ProtectedRoute path="/projects" component={ProjectsPage} />
-          <ProtectedRoute path="/users" component={UsersPage} />
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+                <ProtectedRoute path="/" component={DashboardPage} exact/>
+                <ProtectedRoute path="/projects" component={ProjectsPage} exact/>
+                <ProtectedRoute path="/users" component={UsersPage} exact/>
+            </Switch>
+        </BrowserRouter>
+    </>);
 };
 
 export default index;
