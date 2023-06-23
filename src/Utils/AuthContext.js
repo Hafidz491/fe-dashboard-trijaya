f429259e8fbe4bd9a064f029401494dfe04fbc51
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-06-23 21:35:37 +0700

    Menambahkan context untuk token dan current user

--- src/Utils/AuthContext.js
+++ src/Utils/AuthContext.js
@@ -1,27 +1,30 @@
-import { React, useContext, useState, createContext } from 'react';
+import {React, useContext, useState, createContext} from 'react';
 
 const AuthContext = createContext();
 
 export function useAuth() {
-  return useContext(AuthContext);
+    return useContext(AuthContext);
 }
 
-export function AuthProvider({ children }) {
-  const [token, setToken] = useState(null);
+export function AuthProvider({children}) {
+    const [token, setToken] = useState(null);
+    const [currentUser, setCurrentUser] = useState({});
 
-  function login(token) {
-    setToken(token);
-  }
+    function login(token, dataUser) {
+        setToken(token);
+        setCurrentUser(dataUser)
+    }
 
-  function logout() {
-    setToken('');
-  }
+    function logout() {
+        setToken('');
+    }
 
-  const value = {
-    token,
-    login,
-    logout,
-  };
+    const value = {
+        token,
+        login,
+        logout,
+        currentUser
+    };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
